49b71ee27b411395bc3c3b9feaa8a2cee61cbfea
anthony <anthonys@booj.com>
2020-07-01 12:58:53 -0600

    Updates to home page

--- pages/index.vue
+++ pages/index.vue
@@ -6,12 +6,11 @@
     <div
       class="flex content-center h-screen w-1/2 flex-wrap bg-gray-200 justify-center h-48 overflow-y-auto"
     >
-      <div class="md:w-2/3 mx-auto">
-        <BikeCard v-for="bike in bikes" :bike="bike"/>
+      <div class="md:w-2/3 flex mx-auto">
         <nuxt-link
           to="/bikes/f-650-gs"
           data-test="enter-button"
-          class="w-1/3 p-2 border-2 text-white bg-red-700 border-2 border-red-800 text-center hover:bg-white hover:border-2 hover:text-red-800 shadow-lg"
+          class="w-full p-2 border-2 text-white bg-red-700 border-2 border-red-800 text-center hover:bg-white hover:border-2 hover:text-red-800 shadow-lg"
         >
           ENTER GARAGE
         </nuxt-link>
